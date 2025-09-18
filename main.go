3bc76c6589c6dfdde11772e47d8c376505cb8960
Valery Semenenko <gearmobile@gmail.com>
2025-09-18 19:58:27 +0300

    add reverse method

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
-	//
+	arr := [5]int{1, 2, 3, 4, 5}
+	reverse(&arr)
+	fmt.Println(arr)
+}
+
+func reverse(array *[5]int) {
+	for index, value := range *array {
+		(*array)[len(array)-1-index] = value
+	}
 }
