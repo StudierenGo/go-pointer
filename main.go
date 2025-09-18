d39df2c8d8e1507894ac65c19250780603a04cf6
Valery Semenenko <gearmobile@gmail.com>
2025-09-18 20:38:11 +0300

    start use struct

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type account struct {
+	login    string
+	password string
+	url      string
+}
 
 func main() {
-	arr := [5]int{1, 2, 3, 4, 5}
-	reverse(&arr)
-	fmt.Println(arr)
+	userLogin := promptUserData("Enter your login")
+	userPassword := promptUserData("Enter you password")
+	userUrl := promptUserData("Enter you url")
+
+	account := account{
+		login:    userLogin,
+		password: userPassword,
+		url:      userUrl,
+	}
+
+	outputUserData(account)
+}
+
+func promptUserData(prompt string) string {
+	var userAnswer string
+	fmt.Print(prompt + ": ")
+	fmt.Scan(&userAnswer)
+
+	return userAnswer
+}
+
+func outputUserData(userAccount account) {
+	message := fmt.Sprintf("Dear %v, your password is %v and it's reference to https://%v.com", userAccount.login, userAccount.password, userAccount.url)
+	fmt.Println(message)
 }
 
 func reverse(array *[5]int) {
