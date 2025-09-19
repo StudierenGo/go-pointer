14c14fbee8fdd362f7d22be9ea01e73ae50240d1
Valery Semenenko <gearmobile@gmail.com>
2025-09-19 21:14:17 +0300

    update

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+	"unicode"
 )
 
-type account struct {
+type userAccount struct {
 	login    string
 	password string
 	url      string
@@ -15,30 +19,42 @@ func main() {
 	userPassword := promptUserData("Enter you password")
 	userUrl := promptUserData("Enter you url")
 
-	account := account{
+	account := userAccount{
 		login:    userLogin,
 		password: userPassword,
 		url:      userUrl,
 	}
 
-	outputUserData(account)
+	result := outputUserData(account)
+	fmt.Println(result)
 }
 
 func promptUserData(prompt string) string {
-	var userAnswer string
 	fmt.Print(prompt + ": ")
-	fmt.Scan(&userAnswer)
 
+	reader := bufio.NewReader(os.Stdin)
+	userAnswer, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return ""
+	}
+
+	userAnswer = strings.TrimSpace(userAnswer)
 	return userAnswer
 }
 
-func outputUserData(userAccount account) {
-	message := fmt.Sprintf("Dear %v, your password is %v and it's reference to https://%v.com", userAccount.login, userAccount.password, userAccount.url)
-	fmt.Println(message)
+func outputUserData(account userAccount) string {
+	return fmt.Sprintf(
+		"Dear user %s, your password is %s and it's reference to https://%s.com\n", CapitalizeWord(account.login), account.password, account.url)
 }
 
-func reverse(array *[5]int) {
-	for index, value := range *array {
-		(*array)[len(array)-1-index] = value
+func CapitalizeWord(s string) string {
+	if s == "" {
+		return s
 	}
+
+	runes := []rune(strings.ToLower(s))
+	runes[0] = unicode.ToUpper(runes[0])
+	return string(runes)
 }
